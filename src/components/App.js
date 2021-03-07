5adcc8376004ede62f9f69a9b7713fdd904557a2
Arisa <ars.n.277@gmail.com>
2021-03-07 15:26:37 +0900

    Calendar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Calendar from "./Calendar";
-import Posting from './Posting'
+import Posting from "./Posting";
 import "./index.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Title</h1>
+        <header>
+        <h1>React-Diary</h1>
+          <div className="logo">
+            <span className="icon">date_range</span>
+            <span>react calendar</span>
+          </div>
+        </header>
         <Calendar />
         <Posting />
       </div>
